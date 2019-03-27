35ca85b83b67f6fe524619cdfec982a0af72f737
DonavanWestmoreland <33549773+DonavanWestmoreland@users.noreply.github.com>
2019-03-26 19:43:56 -0500

    added comments

--- Exercise19_8/src/Main.java
+++ Exercise19_8/src/Main.java
@@ -17,16 +17,20 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // arrays
         ArrayList<Integer> ints = new ArrayList<>(asList(1,2,3));
         ArrayList<String> strings = new ArrayList<>(asList("Nebraska","Idaho","California")); 
-
+        // callign generic method
         Main.shuffle(ints);
         Main.shuffle(strings);
     }
     
     public static <E> void shuffle(ArrayList<E> list){
+        //before
         System.out.println(list);
+        //shuffling
         Collections.swap(list, 0,2);
+        //after
         System.out.println(list);
     }
     
